3a4a15b89041614bc91be1995224682047e96565
Sho Nakatani <lay.sakura@gmail.com>
2017-04-02 01:38:43 +0900

    改行

--- akkademy-db/src/test/scala/com/github/laysakura/test/akkademy/AkkademyDbSpec.scala
+++ akkademy-db/src/test/scala/com/github/laysakura/test/akkademy/AkkademyDbSpec.scala
@@ -8,6 +8,7 @@ import org.scalatest.{BeforeAndAfterEach, FunSpecLike, Matchers}
 
 class AkkademyDbSpec extends FunSpecLike with Matchers with BeforeAndAfterEach {
   implicit val system = ActorSystem()
+
   describe("akkademyDb") {
     describe("given SetRequest") {
       it("should place key/value into map") {
